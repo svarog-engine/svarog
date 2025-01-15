3407f391c96313af77a0521a8026929ab2ddf4c5
Miroslav <gavrilov.miroslav@gmail.com>
2025-01-15 02:31:46 +0100

    SparseMatrix rehash

--- SparseMatrix.cs
+++ SparseMatrix.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace svarog
+﻿namespace svarog
 {
     public class SparseMatrix<T>
     {
         public int Width { get; private set; }
         public int Height { get; private set; }
         public long MaxSize { get; private set; }
-        public long Count { get { return _cells.Count; } }
+        public long Count { get { return m_Cells.Count; } }
 
-        private Dictionary<long, T> _cells = new Dictionary<long, T>();
-
-        private Dictionary<int, Dictionary<int, T>> _rows =
-            new Dictionary<int, Dictionary<int, T>>();
-
-        private Dictionary<int, Dictionary<int, T>> _columns =
-            new Dictionary<int, Dictionary<int, T>>();
+        private Dictionary<long, T> m_Cells = new();
+        private Dictionary<int, Dictionary<int, T>> m_Rows = new();
+        private Dictionary<int, Dictionary<int, T>> m_Columns = new();
 
         public SparseMatrix(int w, int h)
         {
@@ -31,33 +21,31 @@ namespace svarog
         public bool IsCellEmpty(int row, int col)
         {
             long index = row * Width + col;
-            return _cells.ContainsKey(index);
+            return m_Cells.ContainsKey(index);
         }
 
-        public T this[int row, int col]
+        public T? this[int row, int col]
         {
             get
             {
                 long index = row * Width + col;
-                T result;
-                _cells.TryGetValue(index, out result);
+                m_Cells.TryGetValue(index, out T? result);
                 return result;
             }
             set
             {
                 long index = row * Width + col;
-                _cells[index] = value;
+                m_Cells[index] = value;
 
-                UpdateValue(col, row, _columns, value);
-                UpdateValue(row, col, _rows, value);
+                UpdateValue(col, row, m_Columns, value);
+                UpdateValue(row, col, m_Rows, value);
             }
         }
 
         private void UpdateValue(int index1, int index2,
             Dictionary<int, Dictionary<int, T>> parent, T value)
         {
-            Dictionary<int, T> dict;
-            if (!parent.TryGetValue(index1, out dict))
+            if (!parent.TryGetValue(index1, out var dict))
             {
                 parent[index2] = dict = new Dictionary<int, T>();
             }
