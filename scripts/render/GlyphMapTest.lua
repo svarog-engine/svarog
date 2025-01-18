df5304f0a6c88b43d443221ba2caa0c4794c6e2a
Miroslav <gavrilov.miroslav@gmail.com>
2025-01-18 02:27:43 +0000

    Nicer rand methods

--- scripts/render/GlyphMapTest.lua
+++ scripts/render/GlyphMapTest.lua
@@ -3,7 +3,7 @@ Engine.OnStartup(function()
 	local alp = "thequickbrownfoxjumpsoverthelazydog"
 	for i = 0, Options.WorldWidth - 1 do
 		for j = 0, Options.WorldHeight - 1 do
-			Glyphs[i][j].Presentation = Rand:Get(alp)
+			Glyphs[i][j].Presentation = Rand:From(alp)
 			Glyphs[i][j].Foreground = Rand:Color()
 			Glyphs[i][j].Background = Rand:Color()
 		end
--- utility/Randomness.cs
+++ utility/Randomness.cs
@@ -9,10 +9,10 @@ namespace svarog.utility
         private Colors Colors = new();
         private System.Random Internal = new();
 
-        public int Get(int min, int max) => min + Internal.Next(max);
-        public string Get(string ts) => ts[Get(0, ts.Length)].ToString();
-        public T Get<T>(T[] ts) => ts[Get(0, ts.Length)];
-        public T Get<T>(List<T> ts) => ts[Get(0, ts.Count)];
+        public int Range(int min, int max) => min + Internal.Next(max);
+        public string From(string ts) => ts[Range(0, ts.Length)].ToString();
+        public T From<T>(T[] ts) => ts[Range(0, ts.Length)];
+        public T From<T>(List<T> ts) => ts[Range(0, ts.Count)];
         public Color Color() => Colors.Random;
     }
 }
