a214df3f4bce854f02fe0639c8f08ad6236a4dc8
Miroslav <gavrilov.miroslav@gmail.com>
2025-03-02 18:11:02 +0000

    GUI diary refactored

---
+++ scripts/gameplay/render/DiaryRender.lua
@@ -0,0 +1,11 @@
+﻿
+local DiaryRenderSystem = Engine.RegisterUIRenderSystem("Diary Render");
+
+function DiaryRenderSystem.Render(ui)
+	ui.PushBox(1, Config.Height - 2, Config.Width, 2)
+		ui.PushOrder("|")
+			local diary = DiaryEntity[Diary]
+			ui.Label(diary.log[diary.index])
+		ui.PopOrder()
+	ui.PopBox()
+end
